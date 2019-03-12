62964e295774b383557e14e65bb0b9ebe7e1343f
moudroff <moudroff>
2019-03-12 11:23:39 +0300

    RoutingApp

---
+++ RoutingApp/RoutingApp/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RoutingApp.Models;
+
+namespace RoutingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            ViewData["Category"] = RouteData.Values["cat"]?.ToString();
+            ViewData["Id"] = RouteData.Values["id"]?.ToString();
+
+
+            return View();
+        }
+
+
+        [Route("ContactPage")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("ContactPage/{name}")]
+        public IActionResult Contact(string name)
+        {
+            ViewData["Message"] = $"Your contact page.  {name}";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RoutingApp/RoutingApp/Startup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RoutingApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            var routeBuilder = new RouteBuilder(app);
+
+            routeBuilder.MapRoute("{controller}/{action}",
+                async context => {
+                    context.Response.ContentType = "text/html; charset=utf-8";
+                    await context.Response.WriteAsync("двухсегментный запрос");
+                });
+
+
+            routeBuilder.MapRoute("{controller}/{action}/{id}",
+                async context => {
+                    context.Response.ContentType = "text/html; charset=utf-8";
+                    await context.Response.WriteAsync("трехсегментный запрос");
+                });
+
+
+            routeBuilder.MapRoute("{controller}/{action}/{categ}/{id}",
+             async context => {
+                 context.Response.ContentType = "text/html; charset=utf-8";
+                 await context.Response.WriteAsync("четырёхсегментный запрос ");
+             });
+
+
+
+            //app.UseRouter(routeBuilder.Build());
+
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("apicat", "api/get/{cat}/{id}", new { controller = "Home", action = "About"});
+
+                routes.MapRoute("api", "api/get", new { controller = "Home", action = "About" });
+
+                
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}

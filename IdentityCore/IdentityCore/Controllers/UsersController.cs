a2ea9fffb6082f871093efd051e02fa209c02dc8
moudroff <moudroff>
2019-03-26 11:06:17 +0300

    Identity Core

---
+++ IdentityCore/IdentityCore/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityCore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityCore.Controllers
+{
+    public class UsersController : Controller
+    {
+        UserManager<User> _userManager;
+
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index() => View(_userManager.Users.ToList());
+
+    }
+}
---
+++ IdentityCore/IdentityCore/Data/Migrations/20190319082754_User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityCore.Data.Migrations
+{
+    public partial class User : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "AspNetUsers");
+        }
+    }
+}

cc65229d99275b68287babbd3e82b5269421c5ed
moudroff <moudroff>
2020-04-21 15:36:25 +0300

    Web Api 811

---
+++ WebApi811/WebApi811/Models/ScoresContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi811.Models
+{
+    public class ScoresContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Score> Scores { get; set; }
+        public ScoresContext(DbContextOptions<ScoresContext> options)
+            : base(options)
+        { }
+
+    }
+}

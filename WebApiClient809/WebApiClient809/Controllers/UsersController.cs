3b20f0ff649227630297d702bd4a832b064b632e
moudroff <moudroff>
2020-04-22 12:58:30 +0300

    WebApi Client 809

---
+++ WebApiClient809/WebApiClient809/Controllers/UsersController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiClient809.Services;
+
+namespace WebApiClient809.Controllers
+{
+    public class UsersController : Controller
+    {
+        UsersService serv = new UsersService();
+        
+        // GET: Users
+        public async Task<ActionResult> Index()
+        {
+            return View(await serv.GetUsers());
+        }
+
+        // GET: Users/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+
+            return View(await serv.GetUsersByID(id));
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApiClient809/WebApiClient809/Services/UsersService.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApiClient809.Models;
+
+namespace WebApiClient809.Services
+{
+    public class UsersService
+    {
+        HttpClient client = new HttpClient();
+        string uri = "http://localhost:63680/api/users";
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var resp = await client.GetAsync(uri);
+            var result = resp.Content.ReadAsStringAsync().Result;
+            var users = JsonConvert.DeserializeObject<List<User>>(result);
+            return users;
+        }
+        public async Task<User> GetUsersByID(int id)
+        {
+            var resp = await client.GetAsync(uri + $"/{id}");
+            var result = resp.Content.ReadAsStringAsync().Result;
+            var user = JsonConvert.DeserializeObject<User>(result);
+            return user;
+        }
+        /*public async Task<HttpStatusCode> AddUser(User user)
+        {
+            HttpResponseMessage response = await client.PostAsync(
+                uri, user);
+            return response.StatusCode;
+
+        }*/
+
+
+
+
+
+    }
+}
